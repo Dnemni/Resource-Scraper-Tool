04c57b10595080c37764f6d1aebccc87d1f1d2e4
Dhruv <dhruvnemani@gmail.com>
2025-05-26 15:02:27 -0700

    Fix API routing configuration

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from resource_scraper.api import app
 
 # Need to explicitly set root_path for Vercel deployment
-app.root_path = "/api"
+app.root_path = ""
 
 # Export for Vercel
 app = app
